39510c69efa6aa3759c769e44255573bf8ca53a1
YA2JA <41586062+YA2JA@users.noreply.github.com>
2019-06-19 21:04:31 +0200

    NATHING
    NOPE

---
+++ first version/learn.py
@@ -0,0 +1,173 @@
+import numpy as np
+from pybrain3.datasets import SupervisedDataSet
+import os, math
+
+alphas = [10**4]
+# подсчитаем нелинейную сигмоиду
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+    
+def cycle():
+        lerns = SupervisedDataSet(1024, 2)
+        do_R = lb.analyse()
+        for i in range(1,104):
+            adress = "folder 1/"+str(i)+".jpg"
+            data = do_R.transform_in_data(adress)
+            lerns.addSample((data), (1,0))
+
+        for i in range(1,106):
+            adress = "folder 2/"+str(i)+".jpg"
+            data = do_R.transform_in_data(adress)
+            lerns.addSample((data), (0,0))
+
+        for i in range(1,75):
+        	adress = "folder 3/"+str(i)+".jpg"
+        	data = do_R.transform_in_data(adress)
+        	lerns.addSample((data), (0,1))
+
+        for i in range(1,66):
+            adress = "folder 4/"+str(i)+".jpg"
+            data = do_R.transform_in_data(adress)
+            lerns.addSample((data), (1,1))
+        return lerns
+
+
+# преобразуем результат сигмоиды к производной
+def sigmoid_output_to_derivative(output):
+    return output*(1-output)
+
+def lost_use():
+    file = open("info_about.txt", "r")
+    data = file.read()
+    to_return = ""
+    N = 0
+    for i in data:
+        if i == ":":
+            N+=1
+        elif N==3:
+            to_return+=i
+
+    return float(to_return)
+
+now = cycle()
+
+X = np.array(now["input"])
+
+y = np.array(now["target"])
+
+try:
+    minPro_error = lost_use()
+except:
+    minPro_error = 1
+
+ask = True
+
+if ask:
+	
+	try:
+		layer_1 = np.load("layer_1.npy")
+		layer_2 = np.load("layer_2.npy")
+		synapse_0 = np.load("W_0.npy")
+		synapse_1 = np.load("W_1.npy")
+	except:
+		print("You  don't have any saves")
+		ask = False
+
+nember_training = 90000
+
+for alpha in alphas:
+
+    print ("\nТренируемся при Alpha:" + str(alpha))
+    #np.random.seed(1)
+    if not ask:
+    	# случайная инициализация весов со средним 0
+    	synapse_0 = 2*np.random.random((1024,200)) - 1
+    	synapse_1 = 2*np.random.random((200,2)) - 1
+    
+    for j in range(nember_training):
+
+        # Прямое распространение по уровням 0, 1 и 2
+        layer_0 = X
+   #    layer_1 = sigmoid(np.dot(layer_0,synapse_0))
+   #    layer_2 = sigmoid(np.dot(layer_1,synapse_1))
+        if not ask:
+        	layer_1 = sigmoid(np.dot(layer_0,synapse_0))
+        	layer_2 = sigmoid(np.dot(layer_1,synapse_1))
+        ask = False
+        # как сильно ошиблись?
+        
+        try:
+
+        	layer_2_error = layer_2 - y
+        
+        except:
+            layer_1 = sigmoid(np.dot(layer_0,synapse_0))
+            layer_2 = sigmoid(np.dot(layer_1,synapse_1))
+
+            layer_2_error = layer_2 - y
+        
+        
+        
+        Pro_error = np.mean(np.abs(layer_2_error))
+
+
+        if (j% 1000) == 0:
+            print("Ошибка после "+str(j)+" повторений:" + str(Pro_error))
+            if Pro_error<minPro_error:
+                minPro_error = Pro_error
+                info_about = open("info_about.txt", "w")
+                info_about.write("Alpha:"+str(alpha)+
+                				"\nTotal nember of training :"+str(j)+
+                				"\nMin of errors:" + str(Pro_error))
+                info_about.close()
+
+                np.save("W_0", synapse_0)
+                np.save("W_1", synapse_1)
+                np.save("layer_1", layer_1)
+                np.save("layer_2", layer_2)
+
+            if Pro_error==minPro_error:
+                continue
+
+
+
+        # в каком направлении цель?
+        # уверены ли мы? Если да, то не нужно слишком сильных изменений
+        layer_2_delta = layer_2_error*sigmoid_output_to_derivative(layer_2)
+
+        # насколько каждое значение из l1 влияет на ошибку в l2 (в соответствии с весами)?
+        layer_1_error = layer_2_delta.dot(synapse_1.T)
+
+        # в каком направлении цель l1?
+        # уверены ли мы? Если да, то не нужно слишком сильных изменений
+        layer_1_delta = layer_1_error * sigmoid_output_to_derivative(layer_1)
+
+        synapse_1 -= alpha * (layer_1.T.dot(layer_2_delta))
+        synapse_0 -= alpha * (layer_0.T.dot(layer_1_delta))
+
+class input_image():
+
+	def transform_in_data(self, adress):
+		img = self.__img_read(adress)
+		one = []
+		for y in img:
+			for x in y:
+				one.append(x)
+		return one
+
+	def __img_read(self, adress):
+		file = Image.open(adress)
+		img_convert = file.convert("L")
+		data = np.asarray(img_convert)
+		resultat = (data > 200) * 1 
+		return resultat
+
+
+
+def dot_sigmoid(self, x, y):
+	inputs = np.dot(x, y)
+	output = 1/(1+np.exp(-inputs))
+	return output
+
+if __name__ == '__main__':
+	main()
--- learnig 2.py
+++ learnig 2.py
@@ -2,34 +2,30 @@ import os, math
 import numpy as np
 from PIL import Image
 from pybrain3.datasets import SupervisedDataSet
+import pandas as pd
 
 										#THIS PARTIE OF CODE IS NOT ENDED!!!
-
-class Neuron_network():
-
+class download_data_to_learn():
+	
 	def __init__(self):
 		self.learnig_data_placement = os.path.dirname(os.path.realpath(__file__))+'/learnig_data/Folder'
-		self.all_right_answer = ['0,0','0,1','1,0','1,1']
 		self.all_files = []
 		self.imgs_arrays = [[],[],[],[]]
 		self.weights = SupervisedDataSet(1024, 2)
-
-class reading_imgs(Neuron_network):
 		
 	def Found_Learnig_Filles(self):
 		for i in range(4):
 			self.all_files.append(os.listdir(self.learnig_data_placement+str(i)))
 
-	def add_information(self,resultat_nember=0):
+	def add_learning_information(self,resultat_nember=0):
 		img_adresse = self.all_files[resultat_nember]
-		right_answer = self.all_right_answer[resultat_nember]
 		for image_name in img_adresse:
 			img = self.binnarizing_img(self.learnig_data_placement+str(resultat_nember)+'/'+image_name)
 			self.imgs_arrays[resultat_nember].append(np.asarray(img))
 
 		for img_nember in range(len(self.all_files[resultat_nember])-1):	
 			end_response = self.flatting_arrays(self.imgs_arrays[resultat_nember][img_nember].tolist())
-			self.weights.addSample((end_response), self.tranlater(right_answer))
+			self.weights.addSample((end_response), self.tranlater(resultat_nember))
 
 	def binnarizing_img(self, img_adresse):
 		file = Image.open(img_adresse)
@@ -43,32 +39,27 @@ class reading_imgs(Neuron_network):
 		return flatten(array)
 
 	def tranlater(self, x):
-		if '0,0' == x:
-			return 0,0
-		if '0,1'== x:
-			return 0,1
-		if '1,0' == x:
-			return 1,0
-		if '1,1'== x:
-			return 1,1 
+		binar_number = '{0:02b}'.format(x)
+		return [int(i) for i in list(binar_number)]
 
-class learning(Neuron_network):
+class learning():
 
 	def __init__(self, weights):
-		self.alphas = [ 1/10**i for i in range(5,-5,-1)]
+		self.alphas = [ 1/10**i for i in range(4,-15,-1)]
 		self.start_layer = np.array(weights['input'])
 		self.right_answer = np.array(weights['target'])
-		self.nember_training = 5000
+		self.nember_training = 9000
+		self.min_Error = 0.99
 
 	def trening(self):
 
 		synapse_0 = 2*np.random.random((1024,256))-1
 		synapse_1 = 2*np.random.random((256,2))-1
-		min_Error = 0.99
+
 		for alpha in self.alphas:
 			print("\nAlpha:" + str(alpha))
 
-			for j in range(self.nember_training):
+			for j in range(int(self.nember_training)):
 				layer_0 = self.start_layer
 				layer_1 = self.sigmoid(np.dot(layer_0,synapse_0))
 				layer_2 = self.sigmoid(np.dot(layer_1,synapse_1))
@@ -76,9 +67,11 @@ class learning(Neuron_network):
 				Pro_error = np.mean(np.abs(layer_2_error))
 				if (j% 1000) == 0:
 					print("Ошибка после "+str(j)+" повторений:" + str(Pro_error))
-					if min_Error == Pro_error: continue
 
-					if min_Error > Pro_error: min_Error = Pro_error 
+					if self.min_Error > Pro_error: 
+						self.min_Error = Pro_error
+						self.__save_learn_data__(synapse_0,synapse_1)
+
 
 				layer_2_delta = layer_2_error*self.sigmoid_output_to_derivative(layer_2)
 
@@ -89,7 +82,7 @@ class learning(Neuron_network):
 				synapse_1 -= alpha * (layer_1.T.dot(layer_2_delta))
 				synapse_0 -= alpha * (layer_0.T.dot(layer_1_delta))
 
-			self.nember_training += 1000
+			
 		
 	def dot_sigmoid(self, x, y):
 		inputs = np.dot(x, y)
@@ -102,17 +95,23 @@ class learning(Neuron_network):
 	def sigmoid_output_to_derivative(self, output):
 		return output*(1-output)
 
+	def __save_learn_data__(self, synapse_0, synapse_1):
+		np.save("W_0", synapse_0)
+		np.save("W_1", synapse_1)
+
 
 
 def main():
-	dowload_img_base = reading_imgs()
-	dowload_img_base.Found_Learnig_Filles()
-	for folder_number in range(4):
-		dowload_img_base.add_information(folder_number)
-	network = learning(dowload_img_base.weights)
+	
+	data = download_data_to_learn()
+	data.Found_Learnig_Filles()
+	for answer_nember in range(4):
+		data.add_learning_information(answer_nember)
+	
+	network = learning(data.weights)
 	network.trening()
-	input()
 
 
 if __name__ == '__main__':
 	main()
+	
